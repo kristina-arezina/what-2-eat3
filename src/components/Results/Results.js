bc4de6b226ae25ac9556bd4cb6aea396ae797c69
Kristina Arezina <arezina.kristina23@gmail.com>
2019-12-03 16:14:20 -0500

    I want an about page

--- src/components/Results/Results.js
+++ src/components/Results/Results.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
-import Typography from '@material-ui/core/Typography';
 import Results2 from "./Results2"
-import AppBar from '@material-ui/core/AppBar';
 
 
 
