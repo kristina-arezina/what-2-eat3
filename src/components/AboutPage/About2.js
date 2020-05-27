d3874c1280ac8ba6c6258dc2784cdbbc3e690c87
Kristina Arezina <kristinaarezina@Kristinas-MacBook-Pro.local>
2020-05-27 17:28:19 -0400

    added images

--- src/components/AboutPage/About2.js
+++ src/components/AboutPage/About2.js
@@ -1,10 +1,12 @@
 import React from "react";
-import ResultsDrawer from "../Results/Drawer.js"; 
+import ResultsDrawer from "../Results/Drawer.js";
+import SingleLineGridList from "./Pictures.js"; 
 
 function About() {
     return(
         <div>
             <ResultsDrawer/>
+            <SingleLineGridList/>
         </div>
     )
 }
---
+++ src/components/AboutPage/TitleData.js
@@ -0,0 +1,17 @@
+import almonds from "./images/almonds.jpg";
+import apples from "./images/apples.jpg";
+
+const tileData = [
+    {
+        img: almonds,
+        title: "Almonds",
+        author: "Jerry",
+    },
+    {
+        img: apples,
+        title: "Apples",
+        author: "Smith",
+    },
+]
+
+export default tileData; 
