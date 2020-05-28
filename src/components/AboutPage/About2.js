89c1e198799974dcb0b460a427726401ad2f8f5b
Kristina Arezina <kristinaarezina@Kristinas-MacBook-Pro.local>
2020-05-28 09:50:14 -0400

    added process explntion

--- src/components/AboutPage/About2.js
+++ src/components/AboutPage/About2.js
@@ -2,12 +2,14 @@ import React from "react";
 import ResultsDrawer from "../Results/Drawer.js";
 import SingleLineGridList from "./Pictures.js"; 
 import TextAbout from "./TextAbout.js";
+import VerticalLinearStepper from "./HowItWorks"
 
 function About() {
     return(
         <div>
             <SingleLineGridList/>
             <ResultsDrawer/>
+            <VerticalLinearStepper/>
             <TextAbout/>
             
         </div>
