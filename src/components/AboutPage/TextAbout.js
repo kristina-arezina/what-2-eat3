6fc26ffb6e1bd3e557b39c09af5bd75ebe985493
Kristina Arezina <kristinaarezina@Kristinas-MacBook-Pro.local>
2020-05-28 08:46:59 -0400

    adding to home page

--- src/components/AboutPage/TextAbout.js
+++ src/components/AboutPage/TextAbout.js
@@ -4,18 +4,35 @@ import Switch from '@material-ui/core/Switch';
 import Paper from '@material-ui/core/Paper';
 import Grow from '@material-ui/core/Grow';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Button from '@material-ui/core/Button';
+
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    height: 180,
-    marginLeft:"20%"
+    height: 1000,
+    marginLeft:"10%"
   },
   container: {
     display: 'flex',
   },
   paper: {
     margin: theme.spacing(1),
+    height: 500,
+    width:"70%",
   },
+  paper2: {
+    margin: theme.spacing(1),
+    height: 400,
+    width:"50%",
+  },
+  toggle:{
+    color:'#f4f4f4',
+  },
+  button: {
+    marginTop:'2%',
+    marginLeft:'calc(50% - 200px)',
+    backgroundColor: "#2E0854"
+  }
 }));
 
 export default function TextAbout() {
@@ -28,10 +45,14 @@ export default function TextAbout() {
 
   return (
     <div className={classes.root}>
+      <Button variant="contained" className={classes.button}>
       <FormControlLabel
-        control={<Switch checked={checked} onChange={handleChange} />}
+      className={classes.toggle} 
+        control={<Switch className={classes.toggle} checked={checked} onChange={handleChange} />}
         label="What is What2Eat?"
       />
+      </Button>
+      
       <div className={classes.container}>
         <Grow in={checked}>
           <Paper elevation={4} className={classes.paper}>
@@ -41,6 +62,17 @@ export default function TextAbout() {
           </Paper>
         </Grow>
         {/* Conditionally applies the timeout prop to change the entry speed. */}
+        <Grow
+          in={checked}
+          style={{ transformOrigin: '0 0 0' }}
+          {...(checked ? { timeout: 1000 } : {})}
+        >
+          <Paper elevation={4} className={classes.paper2}>
+            <svg className={classes.svg}>
+              <polygon />
+            </svg>
+          </Paper>
+        </Grow>
         <Grow
           in={checked}
           style={{ transformOrigin: '0 0 0' }}
